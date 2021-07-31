4c7b4baea0e441e0bf264d4ad8b4451c42ce2b7d
salaheddinechehbi <chehbi.se@gmail.com>
2021-07-31 17:03:19 +0100

    Arrow Function This V2

--- arrowFunction.js
+++ arrowFunction.js
@@ -15,4 +15,25 @@ let resultParam1 = (param = 1) => param * 5
 /* console.log(resultParam1())
 console.log(resultParam1(2)) */
 
+// Whene using arrow function this value belonge to the block that contain the arrow function
+// arrow function does not have it oun This
+function User(){
+    this.age = 0;
+    setInterval(()=> {
+        this.age++;
+        console.log(this.age);
+    }, 1000);
+}
+//let user = new User();
+
+//without using Arrow function
+function Person(){
+    let that = this;
+    that.age = 0;
+    setInterval(function() {
+        that.age++;
+        console.log(that.age);
+    }, 1000);
+}
+let person = new Person();
 
