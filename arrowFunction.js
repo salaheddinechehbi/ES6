49f18082d3750cf24c5a4076300f9cec8ea1c36e
salaheddinechehbi <chehbi.se@gmail.com>
2021-07-31 16:51:34 +0100

    Arrow Function V1

--- arrowFunction.js
+++ arrowFunction.js
@@ -3,6 +3,16 @@
 let result = () => {
     return 45 + 45
 }
-let result2 = () => 40 + 40
+//console.log(result())
 
+let result2 = () => 40 + 40
 //console.log(result2())
+
+let resultParam = (param) => param * 5 
+//console.log(resultParam(10))
+
+let resultParam1 = (param = 1) => param * 5
+/* console.log(resultParam1())
+console.log(resultParam1(2)) */
+
+
